2eb7eac61e055eeb6d1176ea989cdec59aaff832
Luke Wilson <78549037+luketwilson76@users.noreply.github.com>
2022-11-29 08:44:48 -0500

    Boids2.0

---
+++ Boids 2.0/Assets/Scripts/Boid.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+    BoidSettings boidSettings;
+    // State of boid
+    [HideInInspector] public Vector3 position;
+    [HideInInspector] public Vector3 forward;
+    Vector3 velocity;
+    // Update boids:
+    Vector3 acceleration;
+    [HideInInspector] public Vector3 avgFlockDir;
+    [HideInInspector] public Vector3 avgAvoidanceHeading;
+    [HideInInspector] public Vector3 centreOfFlockmates;
+    [HideInInspector] public int numPerceivedFlockmates;
+    // Cached information
+    Material boidMaterial;
+    Transform boidTransform;
+    Transform target;
+
+    void Awake () {
+        boidMaterial = transform.GetComponentInChildren<MeshRenderer> ().material;
+        boidTransform = transform;
+    }
+
+    public void InitializeBoid (BoidSettings settings, Transform target) {
+        this.target = target;
+        this.boidSettings = settings;
+        position = boidTransform.position;
+        forward = boidTransform.forward;
+        float startSpeed = (settings.minSpeed + settings.maxSpeed) / 2;
+        velocity = transform.forward * startSpeed;
+    }
+
+    public void SetColor (Color col) {
+        if (boidMaterial != null) {
+            boidMaterial.color = col;
+        }
+    }
+
+    public void UpdateBoids () {
+        Vector3 acceleration = Vector3.zero;
+
+        if (target != null) {
+            Vector3 offsetToTarget = (target.position - position);
+            acceleration = SteerToward (offsetToTarget) * boidSettings.targetWeight;
+        }
+        if (numPerceivedFlockmates != 0) {
+            centreOfFlockmates /= numPerceivedFlockmates;
+            //new position that adjusts to centre of mass
+            Vector3 offsetToFlockmatesCentre = (centreOfFlockmates - position);
+            //calculates how far apart, how aligned, and how close boids are
+            var alignment = SteerToward (avgFlockDir) * boidSettings.alignWeight;
+            var cohesion = SteerToward (offsetToFlockmatesCentre) * boidSettings.cohesionWeight;
+            var seperation = SteerToward (avgAvoidanceHeading) * boidSettings.seperateWeight;
+            acceleration += alignment;
+            acceleration += cohesion;
+            acceleration += seperation;
+        }
+        if (HeadingForCollision ()) {
+            Vector3 avoidCollisionDir = DetectionRays ();
+            Vector3 avoidCollision = SteerToward (avoidCollisionDir) * boidSettings.avoidCollisionWeight;
+            acceleration += avoidCollision;
+        }
+        velocity += acceleration * Time.deltaTime;
+        float speed = velocity.magnitude;
+        Vector3 dir = velocity / speed;
+        speed = Mathf.Clamp (speed, boidSettings.minSpeed, boidSettings.maxSpeed);
+        velocity = dir * speed;
+        boidTransform.position += velocity * Time.deltaTime;
+        boidTransform.forward = dir;
+        position = boidTransform.position;
+        forward = dir;
+    }
+    Vector3 SteerToward(Vector3 vector)
+    {
+        Vector3 v = vector.normalized * boidSettings.maxSpeed - velocity;
+        return Vector3.ClampMagnitude(v, boidSettings.maxSteerForce);
+    }
+    bool HeadingForCollision () {
+        RaycastHit hit;
+        if (Physics.SphereCast (position, boidSettings.boundsRadius, forward, out hit, boidSettings.collisionAvoidDst, boidSettings.obstacleMask)) {
+            return true;
+        } else { }
+        return false;
+    }
+    Vector3 DetectionRays () {
+        /*create raycasts until one where there's no collison is created
+         use that raycast to */
+        Vector3[] rayDirections = BoidHelper.directions;
+        for (int i = 0; i < rayDirections.Length; i++) {
+            Vector3 dir = boidTransform.TransformDirection (rayDirections[i]);
+            Ray ray = new Ray (position, dir);
+            if (!Physics.SphereCast (ray, boidSettings.boundsRadius, boidSettings.collisionAvoidDst, boidSettings.obstacleMask)) {
+                return dir;
+            }
+        }
+        return forward;
+    }
+}
---
+++ Boids 2.0/Assets/Scripts/BoidManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidManager : MonoBehaviour {
+
+    const int threadGroupSize = 1024;
+
+    public BoidSettings settings;
+    public ComputeShader compute;
+    Boid[] boids;
+
+    void Start () {
+        boids = FindObjectsOfType<Boid> ();
+        foreach (Boid b in boids) {
+            b.InitializeBoid (settings, null);
+        }
+    }
+
+    void Update () {
+        if (boids != null) {
+            //gather list of boids
+            int numBoids = boids.Length;
+            var boidData = new BoidData[numBoids];
+
+            for (int i = 0; i < boids.Length; i++) {
+                boidData[i].position = boids[i].position;
+                boidData[i].direction = boids[i].forward;
+            }
+            //buffer textures based on light refraction
+            var boidBuffer = new ComputeBuffer (numBoids, BoidData.Size);
+            boidBuffer.SetData (boidData);
+            compute.SetBuffer (0, "boids", boidBuffer);
+
+            //sets the radius in which a boid can detect obstacles or other boids
+            compute.SetInt ("numBoids", boids.Length);
+            compute.SetFloat ("viewRadius", settings.perceptionRadius);
+            compute.SetFloat ("avoidRadius", settings.avoidanceRadius);
+            // collecting multiple threads into a single object and manipulating those threads all at once
+            int threadGroups = Mathf.CeilToInt (numBoids / (float) threadGroupSize);
+            compute.Dispatch (0, threadGroups, 1, 1);
+
+            boidBuffer.GetData (boidData);
+            //assign data to each boid to calculate other variables
+            for (int i = 0; i < boids.Length; i++) {
+                boids[i].avgFlockDir = boidData[i].flockHeading;
+                boids[i].centreOfFlockmates = boidData[i].flockCentre;
+                boids[i].avgAvoidanceHeading = boidData[i].avoidanceHeading;
+                boids[i].numPerceivedFlockmates = boidData[i].numFlockmates;
+
+                boids[i].UpdateBoids ();
+            }
+            boidBuffer.Release ();
+        }
+    }
+
+    //struct for each boid object
+    public struct BoidData {
+        public Vector3 position;
+        public Vector3 direction;
+        public Vector3 flockHeading;
+        public Vector3 flockCentre;
+        public Vector3 avoidanceHeading;
+        public int numFlockmates;
+        public static int Size {
+            get {
+                return sizeof (float) * 3 * 5 + sizeof (int);
+            }
+        }
+    }
+}
